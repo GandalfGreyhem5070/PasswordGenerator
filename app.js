bd00a0e7ab55b6a06d6d40a9ce9339626cb5d472
gandalfgreyhem5070 <ashleyaritchie@gmail.com>
2021-10-27 19:53:07 -0400

    Finished styling project in css file. Started on app.js-added functions

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
- 
+//  PASSWORD GENERATOR
+
+// Character Gernerator Functions
+
+// 
+// Function that accepts a string value as an argument and *returns a random index number* from the string argument
+function randomIndex(str){
+    return Math.floor(Math.random() * str.length);
+}
+console.log(randomIndex(`chicken`)); //0,1,2,3,4,5,6 (up to 7 but not including 7)
+// passes `chicken` thru the function
+
+// Function that returns *a random letter* using a random index in the "letters" string
+function getRandomLower(){
+    const letters = `abcdefghijklmnopqrstuvwxyz`;
+    // returning a random letter using a random index in the "letters" string
+    return letters[randomIndex(letters)];
+}
+
+// Example of the getRandomLower function
+console.log(getRandomLower()); //random lowercase letter
+//
+
+//
+function getRandomUpper(){
+    const letters = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    // returning a random letter using a random index in the "letters" string
+    return letters[randomIndex(letters)];
+}
+// Example of the getRandomLower function
+console.log(getRandomUpper()); //random lowercase letter
+//
+
+function getRandomNumber(){
+    const numbers = `123456789`;
+    // returning a random letter using a random index in the "letters" string
+    return numbers[randomIndex(numbers)];
+}
+
+// 
+function getRandomSymbol(){
+    const symbols = `!@#$%^&*(){}[]=<>/,.`;
+    // returning a random letter using a random index in the "letters" string
+    return symbols[randomIndex(symbols)];
+}
+// Example of the getRandomLower function
+console.log(getRandomSymbol()); //random lowercase letter
+//
